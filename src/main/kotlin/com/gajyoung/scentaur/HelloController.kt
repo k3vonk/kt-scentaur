acff4cb857a3fa0a343367675da2e766c1fbb87e
Ga Jun Young <ga.young@personio.de>
2022-06-11 17:43:21 +0100

    Updates greetings endpoint

--- src/main/kotlin/com/gajyoung/scentaur/HelloController.kt
+++ src/main/kotlin/com/gajyoung/scentaur/HelloController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/greet")
     fun index(): String {
         return "Greetings from Spring Boot!"
     }
